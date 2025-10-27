28cffab4530ef11176514d6ac0bc2b324651d204
maurice <m4rius.3d@gmail.com>
2025-10-27 11:07:25 +0200

    modificare eroare

--- app.py
+++ app.py
@@ -10,15 +10,14 @@ ESTOP = False
 
 # ======== CAMERA (Picamera2) ========
 cam = Picamera2()
-# rezoluție bună pentru viteză + compatibilă cu modele AI
+
 cam.configure(cam.create_video_configuration(main={"size": (640, 480)}))
 cam.start()
 
 def mjpeg():
     while True:
         frame = cam.capture_array()
-        # conversie RGB->BGR (necesară dacă vrei să adaugi YOLO / OpenCV ulterior)
-        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+       
 
         cv2.putText(frame, "SmartCar", (10, 25),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
@@ -39,5 +38,4 @@ def video():
 
 # ======== MAIN ========
 if __name__ == '__main__':
-    threading.Thread(target=safety_loop, daemon=True).start()
     app.run(host='0.0.0.0', port=8000, threaded=True)
